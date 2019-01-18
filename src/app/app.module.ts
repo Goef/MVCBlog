6cdb1171d880bb964d9958e64ab195e79d3aab26
Goef <geoffrey0807@hotmail.com>
2019-01-18 15:59:19 +0100

    Auth + firebase beginning

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFireModule} from '@angular/fire/firebase.app.module'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFireStorageModule} from '@angular/fire/storage'
+import { AngularFireAuthModule} from '@angular/fire/auth'
+import { RouterModule, Routes } from '@angular/router'
+
+import { environment } from '../environments/environment'
+ 
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { PostsModule } from './posts/posts.module';
+import { PostService} from './posts/post.service'
+import { fromEventPattern } from 'rxjs';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/blog' , pathMatch: 'full'},
+  {path: '', loadChildren: './posts/posts.module#PostsModule'}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+   BrowserAnimationsModule,
+   RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    CoreModule,
+    SharedModule,
+    PostsModule
+  ],
+  providers: [PostService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/posts/post.ts
@@ -0,0 +1,10 @@
+import { StringFormat } from "@angular/fire/storage/interfaces";
+
+export class Post {
+    title: string
+    author:string
+    authorId:string
+    content:string
+    image:string
+    published: Date
+}
